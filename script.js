30e2fe4b02b34c7a5fc56a40d65caafcfee729b9
Nate Thrash <35614887+natethrash@users.noreply.github.com>
2019-06-26 15:08:56 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function random_number(num) {
 // New function called mash_choice that doesn't take any parameters 
 function mash_choice() {
 	// Since MASH is a special case, give it its own list
-	var mash = ['mansion', 'apartment', 'shack', 'house'];  // The array of choices to pick from 
+	var mash = ['mansion', 'condo', 'yacht', 'house'];  // The array of choices to pick from 
 	var randomNum = random_number(4);  // Use the above function to get a number between 0 and 4
 	return mash[randomNum];  // Return the list item the random number function just picked and exit the function 
 }
@@ -35,16 +35,19 @@ function fill_in_answers(answers) {
 	var answer_2 = document.querySelector('#answer_2');
 	var answer_3 = document.querySelector('#answer_3');
 	var answer_1 = document.querySelector('#answer_1');
+	var answer_4 = document.querySelector('#answer_4');
 	
 	// Fill them with the provided answers
 	home.innerText = answers['mash'];
 	answer_2.innerText = answers['answer_2'];
 	answer_3.innerText = answers['answer_3'];
 	answer_1.innerText = answers['answer_1'];
+	answer_4.innerText = answers['answer_4'];
 	home.innerHTML = answers.mash;  // Change the content of the element in the HTML doc with the id "home" to the "mash" value in answers 
 	answer_2.innerHTML = answers.answer_2;  // Change the content of the element in the HTML doc with the id "career" to the "career" value in answers 
 	answer_3.innerHTML = answers.answer_3;
 	answer_1.innerHTML = answers.answer_1;
+	answer_4.innerHTML = answers.answer_4;
 }
 
 // Button submit handler function
@@ -57,6 +60,7 @@ function handle_submission(evt) {
 	    'mash': mash_choice(),
 	    'answer_2': get_answer('answer_2'),
 	    'answer_3': get_answer('answer_3'),
+	    'answer_4': get_answer('answer_4')
 	    'answer_1': get_answer('answer_1')
 	}
 	// Fill in the answers
